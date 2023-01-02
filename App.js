0b97a3b4f95382f71bac92836e34a8cce85bdeed
Samuel <jorge12sam@gmail.com>
2023-01-02 11:31:55 -0400

    Imagem e estilos

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View >
+    <SafeAreaView >
+      <StatusBar/>
       <Cesta/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
