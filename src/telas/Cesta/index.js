e8491c15e5ad619faf3e246bce02b08b6540b2cf
Samuel <jorge12sam@gmail.com>
2023-01-03 19:49:02 -0400

    Componente de lista

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 
 import Detalhes from './components/Detalhes';
 import Topo from './components/Topo';
+import Itens from './components/Itens';
 
 
-export default function Cesta( {topo, detalhes}){
-    return <>
+export default function Cesta( {topo, detalhes, itens}){
+    return <ScrollView>
     <Topo {...topo}/>
     <View style={estilos.cesta}>
         <Detalhes {...detalhes}/>
+        <Itens {...itens}/>
     </View>
-    </>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create({
